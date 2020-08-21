8e8b3bb4bb4e449e32f1d86eb6f26c56b5f07930
ChrisKnowsDev <chrisharley81@gmail.com>
2020-08-21 14:37:40 -0400

    fetching data, initial styling

---
+++ index.js
@@ -0,0 +1,38 @@
+// drag logo animation effect from popmotion
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector('.logo');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart').start(e => {
+  e.preventDefault();
+  pointer(ballXY.get()).start(ballXY);
+});
+
+listen(document, 'mouseup touchend').start(() => {
+  spring({
+    from: ballXY.get(),
+    velocity: ballXY.getVelocity(),
+    to: { x: 0, y: 0 },
+    stiffness: 200,
+    // mass: 1,
+    // damping: 10
+  }).start(ballXY);
+});
+
+// Fetch dog image
+const dogImg = document.querySelector('.dog-img img');
+const nextBtn = document.querySelector('.next');
+
+const URL = 'https://dog.ceo/api/breeds/image/random';
+
+const fetchDog = () => {
+  fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+      dogImg.src = data.message;
+    });
+};
+
+nextBtn.addEventListener('click', fetchDog);
